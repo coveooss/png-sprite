eb4a9cc88f4d29d3bc1233bff6264f40e2963d6d
Alexandre Paré <alex.pare.inc@gmail.com>
2014-12-12 17:20:05 -0500

    Update gulp.js

--- gulp.js
+++ gulp.js
@@ -1,6 +1,6 @@
 var gutil = require('gulp-util');
 var through = require('through2');
-var Buffer = require('buffer').Buffer;
+var fs = require('fs');
 
 var Sprite = require("./index.js").Sprite;
 
@@ -8,7 +8,7 @@ var path = require('path');
 var PluginError = gutil.PluginError;
 
 
-module.exports = function (opt) {
+module.exports = function(opt) {
   opt = opt || {};
   opt.cssPath = opt.cssPath || './sprite.css';
   opt.pngPath = opt.pngPath || './sprite.png';
@@ -20,38 +20,44 @@ module.exports = function (opt) {
       return; // ignore
     }
 
-    if (opt.base == null) {
+    if (file.isStream()) {
+      return this.emit('error', new PluginError('gulp-sprite',  'Streaming not supported'));
+    }
+
+    if(opt.base == null){
       opt.base = file.base;
     }
 
     sprite.addFile(file, done);
   }
 
-  function endStream(done) {
+  function endStream (done) {
     var self = this;
-    var obj = sprite.compile();
-    self.push(new gutil.File({
+    console.log(path.resolve(opt.base, opt.cssPath), path.resolve(opt.base, opt.pngPath))
+    var obj = sprite.compile(path.relative(path.dirname(path.resolve(opt.base, opt.cssPath)), path.resolve(opt.base, opt.pngPath)));
+    var buffers = [];
+    var stream = obj.png;
+    stream.on('data', function (b) {
+      buffers.push(b);
+    });
+    stream.on('end', function(){
+      var pngFile = new gutil.File({
+        cwd: "/",
+        base: opt.base,
+        path: path.resolve(opt.base, opt.pngPath),
+        contents:Buffer.concat(buffers)
+      });
+      self.push(pngFile);
+      done();
+    })
+
+    var cssFile = new gutil.File({
       cwd: "/",
       base: opt.base,
       path: path.resolve(opt.base, opt.cssPath),
       contents: new Buffer(obj.css)
-    }));
-
-    // through don't like the png stream
-    // just concat the buffer ftw
-    var buffers = [];
-    obj.png.on('data', function(chunk){
-      buffers.push(chunk);
     });
-    obj.png.on('end', function(chunk){
-      self.push(new gutil.File({
-        cwd: "/",
-        base: opt.base,
-        path: path.resolve(opt.base, opt.pngPath),
-        contents: Buffer.concat(buffers)
-      }));
-      done();
-    })
+    this.push(cssFile);
   }
 
   return through.obj(bufferImages, endStream);
